77924a2136c8de9c874a034de00a904400ca9637
Tore Dessena <toredessena@icloud.com>
2020-10-25 09:47:04 +0100

    Initial Commit

---
+++ Estimator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Estimator
+//
+//  Created by salvatore dessena on 25/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
